c9383bac5a3d5eb25e0df36982673cb0da2a00c9
Frank GP <fgp555@gmail.com>
2025-08-29 01:46:58 -0500

    2025-08-29

---
+++ snippets/src/typescriptreact/fgp_rn_touch.tsx
@@ -0,0 +1,6 @@
+<Link href={`/user`} push asChild>
+  <TouchableOpacity /* style={styles.transactionsButton} */>
+    <MaterialIcons name="user" size={18} color="#888" />
+    <Text /* style={styles.actionButtonText} */>user</Text>
+  </TouchableOpacity>
+</Link>;
